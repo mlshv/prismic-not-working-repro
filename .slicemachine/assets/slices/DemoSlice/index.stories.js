b65dc07c5b8b17002e8c14d8f4e3ac484cbfb9c5
Mikhail Malyshev <mikhail.malyshev@pm.bet>
2022-07-04 14:29:29 +0800

    Init

---
+++ .slicemachine/assets/slices/DemoSlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/DemoSlice';
+
+export default {
+  title: 'slices/DemoSlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"demo_slice","items":[],"primary":{"title":[{"type":"heading1","text":"Evolve extensible niches","spans":[]}],"description":[{"type":"paragraph","text":"Adipisicing nostrud dolor exercitation dolor quis sit enim cupidatat cillum nostrud pariatur ad incididunt. Qui ex ea cillum anim reprehenderit labore enim mollit sint veniam velit qui labore elit velit. Exercitation et minim occaecat.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
